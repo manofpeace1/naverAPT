5fb8d15e9e98ce724e474ff5fddea4bae625a1a5
manofpeace1 <sysbobby@gmail.com>
2019-07-08 11:28:47 +0200

    update msg

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ import config
 if __name__ == '__main__':
     script.select_area_code()
     script.select_sale_type()
+
+    print(f"""
+{script.area_name}의
+{script.user_sale_name} 정보를 긁어오는 중...
+    """)
+
     timer_start = default_timer()
-    print("\n" + "긁어오는 중...")
     script.collect_apt_code()
     script.collect_apt_details()
     script.export_to_file()
     timer_end = default_timer()
     time_record = timedelta(seconds=timer_end - timer_start)
+
     print(f"""
     완료! ({time_record})
 
